b7ea52d07ec74c859516a8318b6a59ef426a1446
EmilioKolo <ekolomenski@gmail.com>
2022-06-02 23:05:01 -0300

    Ultimo commit de armado de estructura

--- analisischip/seq.py
+++ analisischip/seq.py
@@ -4,16 +4,9 @@ import time
 import copy
 from random import shuffle
 from pyensembl import EnsemblRelease
-
 # Analisis de secuencias y genomas
 from Bio import Entrez, SeqIO
-Entrez.email = 'ekolomenski@gmail.com';
-Entrez.api_key = '9bccbd4949f226a5930868df36c211e8b408';
 
-mm9 = EnsemblRelease(67, species='mouse');
-hg19 = EnsemblRelease(102, species='human');
-GRCm38 = EnsemblRelease(102, species='mouse');
-dict_genomas = {'mm9':mm9, 'mouse':mm9, 'hg19':hg19, 'human':hg19, 'GRCm38':GRCm38, 'mouse102':GRCm38};
 
 ###################################### CLASES #####################################
 
