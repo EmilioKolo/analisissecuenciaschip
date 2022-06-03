4236fdf2065035588016b22f99fe2827f015c850
EmilioKolo <ekolomenski@gmail.com>
2022-06-02 22:09:33 -0300

    Ultimos cambios de la estructura inicial

--- analisischip/__main__.py
+++ analisischip/__main__.py
@@ -1,8 +1,20 @@
+import logging
 from analisischip import seq_data
 
 '''
 Archivo donde ejecuto las pruebas
+Tipos de error:
+INFO -> 10
+DEBUG -> 20
+WARNING -> 30
+ERROR -> 40
+CRITICAL -> 50
+(usar logging.info, logging.debug, logging.warning, logging.error, logging.critical)
+* Solo mensajes de warning para arriba van a consola
 '''
+
+# Dejar hasta terminar development
+logging.basicConfig(level=logging.DEBUG);
 
 output_dump = [];
 
